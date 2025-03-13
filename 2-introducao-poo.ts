a758839cea800fdbc7a9bc3ef4469f95c410a228
0dayig0r <igormedeiros.f@gmail.com>
2025-03-13 11:00:36 -0300

    introdução ao poo

---
+++ 2-introducao-poo.ts
@@ -0,0 +1,41 @@
+// Sem classe, tem que repetir várias vezes
+const p = {
+  name: "teste",
+  age: 35,
+
+  showName() {
+    // console.log(this.name);
+  },
+};
+
+const anotherP = {
+  name: "Igor",
+  age: 35,
+  showName() {
+    // console.log(this.name);
+  },
+};
+
+p.showName()
+anotherP.showName()
+
+// Com uma classe, podemos instanciar.
+
+class User {
+    name: string = 'Igor';
+    age: number = 55;
+
+    // Metodo para CONSTRUIR um novo USER
+    constructor(name: string, age: number){
+        this.name = name,
+        this.age = age
+    }
+// Metodo de funções
+    showName = () =>{
+        console.log(this.name)
+    }
+}
+
+// Passar os parameetros recebidos no constructor
+const anotherUser = new User('Ana', 33)
+anotherUser.showName()
